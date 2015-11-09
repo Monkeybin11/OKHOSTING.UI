7cf2d9720c2a26f170890d7385056b8ed432161e
David Muñoz <david@okhosting.com>
2015-11-08 20:53:02 -0600

    Added grid and password to UWP

---
+++ src/UWP/OKHOSTING.UI.UWP/Controls/Layouts/Grid.cs
@@ -0,0 +1,103 @@
+﻿using OKHOSTING.UI.Controls;
+
+namespace OKHOSTING.UI.UWP.Controls.Layouts
+{
+    public class Grid : Windows.UI.Xaml.Controls.Grid, UI.Controls.Layouts.IGrid
+    {
+        public Grid()
+        {
+        }
+
+        public int ColumnCount
+        {
+            get
+            {
+                return base.ColumnDefinitions.Count;
+            }
+            set
+            {
+                while (base.ColumnDefinitions.Count < value)
+                {
+                    base.ColumnDefinitions.Add(new Windows.UI.Xaml.Controls.ColumnDefinition());
+                }
+
+                while (base.ColumnDefinitions.Count > value)
+                {
+                    base.ColumnDefinitions.RemoveAt(base.ColumnDefinitions.Count - 1);
+                }
+            }
+        }
+
+        public IPage Page
+        {
+            get
+            {
+                return (Page) System.Windows.Window.GetWindow(this);
+            }
+        }
+
+        public int RowCount
+        {
+            get
+            {
+                return base.RowDefinitions.Count;
+            }
+            set
+            {
+                while (base.RowDefinitions.Count < value)
+                {
+                    base.RowDefinitions.Add(new Windows.UI.Xaml.Controls.RowDefinition());
+                }
+
+                while (base.RowDefinitions.Count > value)
+                {
+                    base.RowDefinitions.RemoveAt(base.RowDefinitions.Count - 1);
+                }
+            }
+        }
+
+        public bool Visible
+        {
+            get
+            {
+                return base.Visibility == Windows.UI.Xaml.Visibility.Visible;
+            }
+            set
+            {
+                if (value)
+                {
+                    base.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                }
+                else
+                {
+                    base.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public IControl GetContent(int row, int column)
+        {
+            foreach (Windows.UI.Xaml.FrameworkElement children in base.Children)
+            {
+                if (Windows.UI.Xaml.Controls.Grid.GetRow(children) == row && Windows.UI.Xaml.Controls.Grid.GetColumn(children) == column)
+                {
+                    return (IControl) children;
+                }
+            }
+
+            return null;
+        }
+
+        public void SetContent(int row, int column, IControl content)
+        {
+            Grid.SetRow((Windows.UI.Xaml.FrameworkElement) content, row);
+            Grid.SetColumn((Windows.UI.Xaml.FrameworkElement) content, column);
+
+            base.Children.Add((Windows.UI.Xaml.FrameworkElement) content);
+        }
+    }
+}
---
+++ src/UWP/OKHOSTING.UI.UWP/Controls/PasswordTextBox.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OKHOSTING.UI.Controls;
+
+namespace OKHOSTING.UI.UWP.Controls
+{
+	public class PasswordTextBox : Windows.UI.Xaml.Controls.StackPanel, IPasswordTextBox
+    {
+        public readonly Windows.UI.Xaml.Controls.PasswordBox NativeTextBox;
+
+        public PasswordTextBox()
+        {
+            NativeTextBox = new Windows.UI.Xaml.Controls.PasswordBox();
+            base.Children.Add(NativeTextBox);
+        }
+
+        public bool Multiline
+        {
+            get
+            {
+                return false;
+            }
+            set
+            {
+            }
+        }
+
+        public IPage Page
+		{
+			get
+			{
+                throw new NotImplementedException();
+			}
+		}
+
+        public string Text
+        {
+            get
+            {
+                return NativeTextBox.Password;
+            }
+            set
+            {
+                NativeTextBox.Password = value;
+            }
+        }
+
+        public bool Visible
+		{
+			get
+			{
+				return base.Visibility == Windows.UI.Xaml.Visibility.Visible;
+			}
+			set
+			{
+				if (value)
+				{
+					base.Visibility = Windows.UI.Xaml.Visibility.Visible;
+				}
+				else
+				{
+					base.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+				}
+			}
+		}
+
+        public void Dispose()
+        {
+        }
+    }
+}
