2efe5c37eebd939320628e0da2c2a31021e7427d
David Muñoz <david@okhosting.com>
2016-01-12 21:25:52 -0600

    add application class

---
+++ src/Xamarin/OKHOSTING.UI.Xamarin.Forms/Application.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OKHOSTING.UI.Xamarin.Forms
+{
+	public class Application : global::Xamarin.Forms.Application
+	{
+		public Application ()
+		{
+			Platform.Current.Page = new Page();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
