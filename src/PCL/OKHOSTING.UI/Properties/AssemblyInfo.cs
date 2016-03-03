cee2d57aa7f1bcca0c50d76ac1ceb30ae7068164
David Muñoz <david@okhosting.com>
2016-03-02 23:43:40 -0600

    All nugets done

--- src/PCL/OKHOSTING.UI/Properties/AssemblyInfo.cs
+++ src/PCL/OKHOSTING.UI/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("OKHOSTING.UI")]
-[assembly: AssemblyDescription("A true multi-platform UI framework. Create your apps once on PCL and have your UI generated on runtime for ASP.NET, Windows Forms, WPF, Linux (WinForms), Mac OS (WinForms and Xamarin.Mac), iOS, Android, Windows Phone and Windows Store (all mobile via Xamarin Forms)")]
+[assembly: AssemblyDescription("Motherfucking cool multi-platform UI framework. Create your apps once on PCL and have your UI generated on runtime for ASP.NET, Windows Forms, WPF, Linux (Mono + WinForms), Mac OS (Mono + WinForms and Xamarin.Mac), iOS, Android, Windows Phone (via Xamarin.Forms) and Windows Universal Platform.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OK HOSTING")]
 [assembly: AssemblyProduct("OKHOSTING.UI")]
