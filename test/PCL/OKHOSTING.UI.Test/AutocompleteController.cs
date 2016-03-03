f8c902cfa1fc3912094b7b473230e2a3feeaa430
adrianpch <dmydweb@gmail.com>
2016-03-03 14:26:13 -0600

    autocomlete

--- test/PCL/OKHOSTING.UI.Test/AutocompleteController.cs
+++ test/PCL/OKHOSTING.UI.Test/AutocompleteController.cs
@@ -6,5 +6,37 @@ namespace OKHOSTING.UI.Test
 {
 	public class AutocompleteController: Controller
     {
+        public override void Start()
+        {
+            base.Start();
+
+            IStack Stack = Platform.Current.Create<IStack>();
+
+            ILabel lblLabel = Platform.Current.Create<ILabel>();
+            lblLabel.Text = "Tis is my team";
+            lblLabel.Height = 30;
+            Stack.Children.Add(lblLabel);
+
+            IAutocomplete aucAutocomplete = Platform.Current.Create<IAutocomplete>();
+            aucAutocomplete.Name = "Team";
+            aucAutocomplete.BackgroundColor = new Color(1, 222, 184, 135);
+            aucAutocomplete.FontColor = new Color(1, 36, 24, 130);
+            aucAutocomplete.BorderColor = new Color(1, 229, 238, 0);
+            aucAutocomplete.Width = 40;
+            aucAutocomplete.FontFamily = "Times new roman";
+
+            IButton cmdClose = Platform.Current.Create<IButton>();
+            cmdClose.Text = "Close";
+            cmdClose.Click += CmdClose_Click;
+            Stack.Children.Add(cmdClose);
+
+            Platform.Current.Page.Title = "Test Autocomplete";
+            Platform.Current.Page.Content = Stack;
+        }
+
+        private void CmdClose_Click(object sender, EventArgs e)
+        {
+            this.Finish();
+        }
     }
 }
