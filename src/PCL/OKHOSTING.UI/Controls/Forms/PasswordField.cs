2d441eb4ee7e4348277fca2c8fb91d9adaba3836
David Muñoz <david@okhosting.com>
2016-04-05 18:11:48 -0500

    Added PasswordField class

---
+++ src/PCL/OKHOSTING.UI/Controls/Forms/PasswordField.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace OKHOSTING.UI.Controls.Forms
+{
+	/// <summary>
+	/// Field for string values
+	/// </summary>
+	public class PasswordField : FormField
+	{
+		public new IPasswordTextBox ValueControl
+		{
+			get
+			{
+				return (IPasswordTextBox) base.ValueControl;
+			}
+			protected set
+			{
+				base.ValueControl = value;
+			}
+		}
+
+		public override object Value
+		{
+			get
+			{
+				return ValueControl.Value;
+			}
+			set
+			{
+				ValueControl.Value = (string) value;
+			}
+		}
+
+		public override Type ValueType
+		{
+			get
+			{
+				return typeof(string);
+			}
+		}
+		
+		/// <summary>
+		/// Creates the controls for displaying the field
+		/// </summary>
+		protected override void CreateValueControl()
+		{
+			ValueControl = Platform.Current.Create<IPasswordTextBox>();
+        }
+	}
+}
