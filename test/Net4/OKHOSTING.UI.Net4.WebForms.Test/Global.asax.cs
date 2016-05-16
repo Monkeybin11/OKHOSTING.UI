eb64a306becfcad7eaed0e15d475c1e0bb05f567
Edgard David Iván Muñoz Chávez <david@okhosting.com>
2016-05-16 18:31:07 -0500

    tabs

--- test/Net4/OKHOSTING.UI.Net4.WebForms.Test/Global.asax.cs
+++ test/Net4/OKHOSTING.UI.Net4.WebForms.Test/Global.asax.cs
@@ -7,7 +7,7 @@ using System.Web.SessionState;
 
 namespace OKHOSTING.UI.Net4.WebForms.Test
 {
-    public class Global : OKHOSTING.UI.Net4.WebForms.Global
-    {
-    }
+	public class Global : OKHOSTING.UI.Net4.WebForms.Global
+	{
+	}
 }
