e67f4ee22a5c3e15ff61c1c7d5440a8e11a8861b
Edgard David Iván Muñoz Chávez <161583+okhosting@users.noreply.github.com>
2019-02-05 13:35:57 -0600

    adding openFile to all platforms

---
+++ src/Xamarin/OKHOSTING.UI.Xamarin.iOS/Media/OpenFile.cs
@@ -0,0 +1,35 @@
+﻿using Foundation;
+using UIKit;
+
+namespace OKHOSTING.UI.Xamarin.iOS.Media
+{
+	public class OpenFile : UI.Media.IOpenFile
+	{
+		public void Open(string fullPath)
+		{
+			var PreviewController = UIDocumentInteractionController.FromUrl(NSUrl.FromFilename(fullPath));
+			PreviewController.Delegate = new UIDocumentInteractionControllerDelegateClass(UIApplication.SharedApplication.KeyWindow.RootViewController);
+			PreviewController.PresentPreview(true);
+		}
+
+		public class UIDocumentInteractionControllerDelegateClass : UIDocumentInteractionControllerDelegate
+		{
+			UIViewController ownerVC;
+
+			public UIDocumentInteractionControllerDelegateClass(UIViewController vc)
+			{
+				ownerVC = vc;
+			}
+
+			public override UIViewController ViewControllerForPreview(UIDocumentInteractionController controller)
+			{
+				return ownerVC;
+			}
+
+			public override UIView ViewForPreview(UIDocumentInteractionController controller)
+			{
+				return ownerVC.View;
+			}
+		}
+	}
+}
