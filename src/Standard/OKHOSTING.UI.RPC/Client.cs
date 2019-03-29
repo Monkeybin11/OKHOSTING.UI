079ec154c00f2fbcdfe0c636b546c36ed775c0d2
Edgard David Iván Muñoz Chávez <161583+okhosting@users.noreply.github.com>
2019-03-29 16:43:46 -0600

    Create Client.cs

---
+++ src/Standard/OKHOSTING.UI.RPC/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OKHOSTING.UI.RPC
+{
+	public class Client
+	{
+		public void Setup()
+		{
+			Core.BaitAndSwitch.PlatformSpecificModifiers.Add(Tuple.Create<Type, Func<object, object>>(typeof(UI.Controls.IClickable), (control) => 
+			{
+				((UI.Controls.IClickable) control).Click += Client_Click;
+				return control;
+			}));
+
+			Core.BaitAndSwitch.PlatformSpecificModifiers.Add(Tuple.Create<Type, Func<object, object>>(typeof(UI.Controls.IInputControl<object>), (control) => 
+			{
+				((UI.Controls.IClickable) control).Click += Client_Click;
+				return control;
+			}));
+		}
+
+		private void Client_Click(object sender, EventArgs e)
+		{
+			//send click to server
+		}
+	}
+}
