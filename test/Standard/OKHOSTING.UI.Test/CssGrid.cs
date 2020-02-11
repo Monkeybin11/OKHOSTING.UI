bc3eb6f55ae23a2164309f5979c9845294932a69
jesusvalm97 <52518344+jesusvalm97@users.noreply.github.com>
2020-02-11 13:57:03 -0600

    Apply cssgrid rules to CSSGrid Controller test

--- test/Standard/OKHOSTING.UI.Test/CssGrid.cs
+++ test/Standard/OKHOSTING.UI.Test/CssGrid.cs
@@ -16,60 +16,65 @@ namespace OKHOSTING.UI.Test
 			/// </summary>
 			protected override void OnStart()
 			{
-				IGrid grid = Core.BaitAndSwitch.Create<IGrid>();
+            IGrid grid = Core.BaitAndSwitch.Create<IGrid>();
+            grid.ColumnCount = 3;
+            grid.RowCount = 3;
+            grid.Name = "grid";
 
-				grid.ColumnCount = 3;
-				grid.RowCount = 3;
+            IButton BtnClose = Core.BaitAndSwitch.Create<IButton>();
+            BtnClose.Text = "Close";
+            BtnClose.Name = "Close";
+            grid.SetContent(0, 0, BtnClose);
+
+            ICalendar Ccalendar = Core.BaitAndSwitch.Create<ICalendar>();
+            Ccalendar.Name = "Calendar";
+            grid.SetContent(0, 1, Ccalendar);
+
+            IDatePicker Pdate = Core.BaitAndSwitch.Create<IDatePicker>();
+            Pdate.Name = "DatePicker";
+            grid.SetContent(0, 2, Pdate);
+
+            ILabelButton ListP = Core.BaitAndSwitch.Create<ILabelButton>();
+            ListP.Text = "ListPicker";
+            grid.SetContent(1, 0, ListP);
+
+            IButton Image = Core.BaitAndSwitch.Create<IButton>();
+            Image.Name = "Picture";
+            Image.Text = "texto";
+            grid.SetContent(1, 1, Image);
+
+            ILabel Label1 = Core.BaitAndSwitch.Create<ILabel>();
+            Label1.Text = "Welcome";
+            Label1.Name = "label1";
+            grid.SetContent(1, 2, Label1);
+
+            ILabelButton labelbutton = Core.BaitAndSwitch.Create<ILabelButton>();
+            labelbutton.Name = "labelButton";
+            labelbutton.Text = "Button";
+            grid.SetContent(2, 0, labelbutton);
+
+            IListPicker picker = Core.BaitAndSwitch.Create<IListPicker>();
+            picker.Name = "picker";
+            grid.SetContent(2, 1, picker);
+
+            ILabel label2 = Core.BaitAndSwitch.Create<ILabel>();
+            label2.Text = "Label2";
+            label2.Name = "Label2";
+            grid.SetContent(2, 2, label2);
+
+            Page.Title = "Hi friend";
+            Page.Content = grid;
+
+            CSS.Style style = new CSS.Style();
+            style.Parse(@"
+
+                #grid 
+                {
+                    display: grid;
+                }
 
 
-				IButton BtnClose = Core.BaitAndSwitch.Create<IButton>();
-				BtnClose.Text = "Close";
-				BtnClose.Name = "Close";
-				grid.SetContent(0, 0, BtnClose);
-
-				ICalendar Ccalendar = Core.BaitAndSwitch.Create<ICalendar>();
-				Ccalendar.Name = "Calendar";
-				grid.SetContent(0, 1, Ccalendar);
-
-				IDatePicker Pdate = Core.BaitAndSwitch.Create<IDatePicker>();
-				Pdate.Name = "DatePicker";
-				grid.SetContent(0, 2, Pdate );
-
-				ILabelButton ListP = Core.BaitAndSwitch.Create<ILabelButton>();
-				ListP.Text = "ListPicker";
-				grid.SetContent(1, 0, ListP);
-
-				IButton Image = Core.BaitAndSwitch.Create<IButton>();
-				Image.Name = "Picture";
-			    Image.Text = "texto";
-				grid.SetContent(1, 1, Image);
-
-				ILabel Label1 = Core.BaitAndSwitch.Create<ILabel>();
-				Label1.Text = "Welcome";
-				Label1.Name = "label1";
-				grid.SetContent(1, 2, Label1);
-
-				ILabelButton labelbutton = Core.BaitAndSwitch.Create<ILabelButton>();
-				labelbutton.Name = "labelButton";
-				labelbutton.Text = "Button";
-				grid.SetContent(2, 0, labelbutton );
-
-				IListPicker picker = Core.BaitAndSwitch.Create<IListPicker>();
-				picker.Name = "picker";
-				grid.SetContent(2, 1, picker);
-
-				ILabel label2 = Core.BaitAndSwitch.Create<ILabel>();
-				label2.Text = "Label2";
-				label2.Name = "Label2";
-				grid.SetContent(2, 2, label2);
-
-				Page.Title = "Hi friend";
-				Page.Content = grid;
-
-			CSS.Style style = new CSS.Style();
-			style.Parse(@"
-			
-			");
-			}
-	}
+            ");
+        }
+    }
 }
