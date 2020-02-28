e09387f7474112747aef8a3bd225ff01037cf411
F38R0N10 <52300407+F38R0N10@users.noreply.github.com>
2020-02-28 10:22:13 -0600

    grid changes of the CssGrid2 class in spelling errors

--- test/Standard/OKHOSTING.UI.Test/TemplateRows.cs
+++ test/Standard/OKHOSTING.UI.Test/TemplateRows.cs
@@ -339,7 +339,7 @@ namespace OKHOSTING.UI.Test
             ITextBox texte001 = Core.BaitAndSwitch.Create<ITextBox>();
             texte001.Name = "texte001";
             texte001.Value = "Primer textbox";
-            GridFive.SetContent(1, 0, texte01);
+            GridFive.SetContent(1, 0, texte001);
 
             ITextBox texte002 = Core.BaitAndSwitch.Create<ITextBox>();
             texte002.Name = "texte002";
@@ -405,7 +405,7 @@ namespace OKHOSTING.UI.Test
             ILabel label004a = Core.BaitAndSwitch.Create<ILabel>();
             label004a.Name = "label004a";
             label004a.Text = "Cuarto label";
-            GridSix.SetContent(0, 3, label004);
+            GridSix.SetContent(0, 3, label004a);
 
             ITextBox texte001a = Core.BaitAndSwitch.Create<ITextBox>();
             texte001a.Name = "texte001a";
@@ -420,7 +420,7 @@ namespace OKHOSTING.UI.Test
             ITextBox texte003a = Core.BaitAndSwitch.Create<ITextBox>();
             texte003a.Name = "texte003a";
             texte003a.Value = "Tercer textbox";
-            GridSix.SetContent(1, 2, texte003);
+            GridSix.SetContent(1, 2, texte003a);
 
             ITextBox texte004a = Core.BaitAndSwitch.Create<ITextBox>();
             texte004a.Name = "texte004a";
@@ -435,7 +435,7 @@ namespace OKHOSTING.UI.Test
             IButton boton002a = Core.BaitAndSwitch.Create<IButton>();
             boton002a.Name = "boton002a";
             boton002a.Text = "Segundo boton";
-            GridSix.SetContent(2, 1, boton002);
+            GridSix.SetContent(2, 1, boton002a);
 
             IButton boton003a = Core.BaitAndSwitch.Create<IButton>();
             boton003a.Name = "boton003a";
@@ -653,7 +653,7 @@ namespace OKHOSTING.UI.Test
             IButton boton003d = Core.BaitAndSwitch.Create<IButton>();
             boton003d.Name = "boton003d";
             boton003d.Text = "Tercer boton";
-            GridNine.SetContent(2, 2, boton003c);
+            GridNine.SetContent(2, 2, boton003d);
 
             IButton boton004d = Core.BaitAndSwitch.Create<IButton>();
             boton004d.Name = "boton004d";
