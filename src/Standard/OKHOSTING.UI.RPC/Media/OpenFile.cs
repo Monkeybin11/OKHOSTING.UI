5fe06688ba2cfc0d9ceb8cd9f2f0ddee4d36e45e
Edgard David Iván Muñoz Chávez <david@okhosting.com>
2020-03-04 13:01:39 -0600

    add openfile to rpc

---
+++ src/Standard/OKHOSTING.UI.RPC/Media/OpenFile.cs
@@ -0,0 +1,20 @@
+﻿using OKHOSTING.UI.Media;
+using OKHOSTING.UI.RPC.Controls;
+
+namespace OKHOSTING.UI.RPC.Media
+{
+	/// <summary>
+	/// Represents a class that can locally open a file (the file itself could be local or remote)
+	/// </summary>
+	public class OpenFile : Control, IOpenFile
+	{
+		/// <summary>
+		/// Locally opens a file for the user to see or edit
+		/// </summary>
+		/// <param name="fullPath">Full path of the file</param>
+		public void Open(string fullPath)
+		{
+			Invoke(nameof(Open), fullPath);
+		}
+	}
+}
