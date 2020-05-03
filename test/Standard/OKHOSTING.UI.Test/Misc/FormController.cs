28d9d91154b2ac75b8939d2dd76885efd96188de
Edgard David Iván Muñoz Chávez <161583+okhosting@users.noreply.github.com>
2020-05-03 11:33:44 -0500

    change in namespace

--- test/Standard/OKHOSTING.UI.Test/Misc/FormController.cs
+++ test/Standard/OKHOSTING.UI.Test/Misc/FormController.cs
@@ -2,7 +2,7 @@
 using OKHOSTING.UI.Controls;
 using OKHOSTING.UI.Controls.Layout;
 using System.IO;
-using OKHOSTING.UI.Forms;
+using OKHOSTING.UI.Controllers.Forms;
 
 namespace OKHOSTING.UI.Test.Misc
 {
