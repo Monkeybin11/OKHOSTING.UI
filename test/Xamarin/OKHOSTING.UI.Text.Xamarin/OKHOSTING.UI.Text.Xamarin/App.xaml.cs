b2931b7685a3cde57d7bb44234372a85772e593e
jesusvalm97 <52518344+jesusvalm97@users.noreply.github.com>
2020-05-25 14:14:09 -0500

    Test Xamarin.Android

---
+++ test/Xamarin/OKHOSTING.UI.Text.Xamarin/OKHOSTING.UI.Text.Xamarin/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿
+using OKHOSTING.UI.Test.Misc;
+
+namespace OKHOSTING.UI.Text.Xamarin
+{
+    public partial class App : global::Xamarin.Forms.Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var page = new UI.Xamarin.Forms.Page();
+            page.App = new UI.App();
+            var index = new IndexController();
+            index.Page = page;
+
+            index.Start();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
--- test/Xamarin/old/OKHOSTING.UI.Xamarin.Forms.Test/OKHOSTING.UI.Xamarin.Forms.Test/App.xaml.cs
+++ test/Xamarin/old/OKHOSTING.UI.Xamarin.Forms.Test/OKHOSTING.UI.Xamarin.Forms.Test/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace OKHOSTING.UI.Xamarin.Forms.Test
+﻿using OKHOSTING.UI.Test;
+
+namespace OKHOSTING.UI.Xamarin.Forms.Test
 {
 	public partial class App : global::Xamarin.Forms.Application
 	{
@@ -8,6 +10,8 @@
 
 			var page = new Page();
 			MainPage = page;
+
+			page.App = new UI.App();
 
 			new UI.Test.IndexController(page).Start();
 		}
