5ddb470a087b6b3d6990e6cfc8542420b94d9baa
Edgard David Iván Muñoz Chávez <161583+okhosting@users.noreply.github.com>
2020-07-08 11:39:33 -0500

    Add activityIndicator for Xamarinf forms

---
+++ src/Standard/OKHOSTING.UI/Controls/IActivityIndicator.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace OKHOSTING.UI.Controls
+{
+	/// <summary>
+	/// Visual control used to indicate that a task is being executed
+	/// </summary>
+	public interface IActivityIndicator: IControl
+	{
+		bool IsRunning { get; set; }
+		Color Color { get; set; }
+	}
+}
---
+++ src/Xamarin/OKHOSTING.UI.Xamarin.Forms/Controls/ActivityIndicator.cs
@@ -0,0 +1,10 @@
+﻿namespace OKHOSTING.UI.Xamarin.Forms.Controls
+{
+	/// <summary>
+	/// Visual control used to indicate that a task is being executed
+	/// </summary>
+	public class ActivityIndicator: global::Xamarin.Forms.ActivityIndicator
+	{
+
+	}
+}
