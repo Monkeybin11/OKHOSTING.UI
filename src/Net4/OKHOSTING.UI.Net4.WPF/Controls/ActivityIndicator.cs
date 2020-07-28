e41078e55fb675256ac502717773086c9cd7b23b
Edgard David Iván Muñoz Chávez <161583+okhosting@users.noreply.github.com>
2020-07-28 15:48:38 -0500

    add ActivityIndicator for wpf

---
+++ src/Net4/OKHOSTING.UI.Net4.WPF/Controls/ActivityIndicator.cs
@@ -0,0 +1,266 @@
+﻿using OKHOSTING.UI.Controls;
+using System;
+using System.Drawing;
+
+namespace OKHOSTING.UI.Net4.WPF.Controls
+{
+	public class ActivityIndicator : System.Windows.Controls.Panel, IActivityIndicator
+	{
+		bool _IsRunning;
+
+		public ActivityIndicator()
+		{
+			Visibility = System.Windows.Visibility.Collapsed;
+		}
+
+		public bool IsRunning
+		{
+			get
+			{
+				return _IsRunning;
+			}
+			set
+			{
+				_IsRunning = value;
+
+				if (value)
+				{
+					System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.WaitCursor;
+				}
+				else
+				{
+					System.Windows.Forms.Cursor.Current = System.Windows.Forms.Cursors.Default;
+				}
+			}
+		}
+
+		public Color Color
+		{
+			get;
+			set;
+		}
+
+		#region IControl
+
+		/// <summary>
+		/// Gets or sets wether the control is visible or not.
+		/// <para xml:lang="es">
+		/// Obtiene o establece si el control es visible o no.
+		/// </para>
+		/// </summary>
+		bool IControl.Visible
+		{
+			get
+			{
+				return base.Visibility == System.Windows.Visibility.Visible;
+			}
+			set
+			{
+				if (value)
+				{
+					base.Visibility = System.Windows.Visibility.Visible;
+				}
+				else
+				{
+					base.Visibility = System.Windows.Visibility.Hidden;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets wether the control is enabled or not
+		/// <para xml:lang="es">
+		/// Obtiene o establece si el control es habilitado o no.
+		/// </para>
+		/// </summary>
+		bool IControl.Enabled
+		{
+			get
+			{
+				return base.IsEnabled;
+			}
+			set
+			{
+				base.IsEnabled = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the width of the control.
+		/// <para xml:lang="es">
+		/// Obtiene o establece el ancho del control.
+		/// </para>
+		/// </summary>
+		double? IControl.Width
+		{
+			get
+			{
+				return base.Width;
+			}
+			set
+			{
+				if (value.HasValue)
+				{
+					base.Width = value.Value;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the height of the control.
+		/// <para xml:lang="es">
+		/// Obtiene o establece la altura del control.
+		/// </para>
+		/// </summary>
+		double? IControl.Height
+		{
+			get
+			{
+				return base.Height;
+			}
+			set
+			{
+				if (value.HasValue)
+				{
+					base.Height = value.Value;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the control margin.
+		/// <para xml:lang="es">
+		/// Obtiene o establece el margen del control.
+		/// </para>
+		/// </summary>
+		Thickness IControl.Margin
+		{
+			get
+			{
+				return Platform.Parse(base.Margin);
+			}
+			set
+			{
+				base.Margin = Platform.Parse(value);
+			}
+		}
+
+		/// <summary>
+		/// Space that this control will set between its content and its border
+		/// <para xml:lang="es">
+		/// Espacio que este control se establecerá entre su contenido y su borde
+		/// </para>
+		/// </summary>
+		Thickness IControl.Padding
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the color of the Control background.
+		/// <para xml:lang="es">
+		/// Obtiene o establece el color de fondo del control.
+		/// </para>
+		/// </summary>
+		Color IControl.BackgroundColor
+		{
+			get
+			{
+				return Platform.Parse(((System.Windows.Media.SolidColorBrush)base.Background).Color);
+			}
+			set
+			{
+				base.Background = new System.Windows.Media.SolidColorBrush(Platform.Parse(value));
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the color of the control border.
+		/// <para xml:lang="es">
+		/// Obtiene o establece el color del borde del control.
+		/// </para>
+		/// </summary>
+		Color IControl.BorderColor
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the width of the control border.
+		/// <para xml:lang="es">
+		/// Obtiene o establece el ancho del borde del control.
+		/// </para>
+		/// </summary>
+		Thickness IControl.BorderWidth
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the control horizontal alignment.
+		/// <para xml:lang="es">
+		/// Obtiene o establece la alineación horizontal del control.
+		/// </para>
+		/// </summary>
+		HorizontalAlignment IControl.HorizontalAlignment
+		{
+			get
+			{
+				return Platform.Parse(base.HorizontalAlignment);
+			}
+			set
+			{
+				base.HorizontalAlignment = Platform.Parse(value);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the control vertical alignment.
+		/// <para xml:lang="es">
+		/// Obtiene o establece la alineación vertical del control.
+		/// </para>
+		/// </summary>
+		VerticalAlignment IControl.VerticalAlignment
+		{
+			get
+			{
+				return Platform.Parse(base.VerticalAlignment);
+			}
+			set
+			{
+				base.VerticalAlignment = Platform.Parse(value);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets a list of classes that define a control's style. 
+		/// Exactly the same concept as in CSS. 
+		/// </summary>
+		string IControl.CssClass { get; set; }
+
+		/// <summary>
+		/// Control that contains this control, like a grid, or stack
+		/// </summary>
+		IControl IControl.Parent
+		{
+			get
+			{
+				return (IControl)base.Parent;
+			}
+		}
+
+		#endregion
+
+		public void Dispose()
+		{
+		}
+
+		object ICloneable.Clone()
+		{
+			return MemberwiseClone();
+		}
+	}
+}
