be4621e3b34b17fdfca18d05e80538273f7b5e03
Edgard David Iván Muñoz Chávez <161583+okhosting@users.noreply.github.com>
2020-08-07 12:25:59 -0500

    replaced OpenFile with Xamarin.Forms generic method

---
+++ src/Xamarin/OKHOSTING.UI.Xamarin.Forms/Media/OpenFile.cs
@@ -0,0 +1,16 @@
+﻿using OKHOSTING.UI.Media;
+using global::Xamarin.Essentials;
+
+namespace OKHOSTING.UI.Xamarin.Forms.Media
+{
+	public class OpenFile : IOpenFile
+	{
+		public async void Open(string fullPath)
+		{
+			await Launcher.OpenAsync(new OpenFileRequest
+			{
+				File = new ReadOnlyFile(fullPath)
+			});
+		}
+	}
+}
